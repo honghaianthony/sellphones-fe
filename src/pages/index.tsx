54e9372c89875086de0464add2ff349004a2afe3
Anthony Hai <namsaoquan@gmail.com>
2022-04-21 22:51:52 +0700

    Add Card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,19 @@ import type { NextPage } from 'next';
 import Navbar from '@/components/Navbar';
 import SubNavbar from '@/components/SubNavbar';
 import Slider from '@/components/Slider';
+import Card from '@/components/Card';
 import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
 	return (
-		<div>
+		<div className="bg-gray-100 h-screen">
 			<Navbar />
 			<SubNavbar />
-			<div className="flex flex-col lg:flex lg:flex-row w-full">
-				<Slider />
+			<Slider />
+			<div className="container my-20 mx-auto items-center justify-between bg-gray-100">
+				<span className="font-bold text-2xl">Hàng nổi bật</span>
+				<Card />
 			</div>
 		</div>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
 	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
-		extend: {},
-		fontFamily: {
-			josefin: ['Josefin Sans', 'sans-serif'],
+		extend: {
+			fontFamily: {
+				josefin: ['Josefin Sans', 'sans-serif'],
+			},
+			colors: {
+				yellow: {
+					50: '#ffc107',
+				},
+				gray: {
+					10: '#bfbfbf',
+				},
+			},
 		},
 	},
 	plugins: [],
