eae81703c4942b7168bc9a85ec2341d31258db60
Duy Nguyễn <nguyenngockhuongduy172@gmail.com>
2022-06-05 10:57:35 +0700

    init employee page

---
+++ src/pages/employee/overview.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import EmployeeLayout from '@/components/Layouts/EmployeeLayout';
+
+function Overview() {
+  return (
+    <EmployeeLayout>
+      <div>EMployee</div>
+    </EmployeeLayout>
+  )
+}
+
+export default Overview;
